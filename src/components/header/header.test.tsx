1201557abe50f591b2e08187deda516883832c5c
Danny Pesses <dcpesses@users.noreply.github.com>
2025-08-07 14:34:05 -0700

    test(header): updated test coverage to 100%

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,4 +1,5 @@
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+import {userEvent} from '@testing-library/user-event';
 import {LinkProps, NavLinkProps, PropsWithChildren} from '@/../tests/mockReactRouterHelper';
 
 import Header from './index';
@@ -35,4 +36,17 @@ describe('Header', () => {
     const {container} = render(<Header />);
     expect(container).toBeDefined();
   });
+  test('Should open and close the "Coming Soon" modal', async() => {
+    render(<Header />);
+    const aboutLink = screen.getByText('About');
+    await userEvent.click(aboutLink);
+    const modalElement = screen.queryByRole('dialog');
+    expect(modalElement).toBeInTheDocument();
+
+    const closeBtn = screen.getByText('Close');
+    await userEvent.click(closeBtn);
+    const modalElement2 = screen.queryByRole('dialog');
+    expect(modalElement2).not.toBeInTheDocument();
+  });
+
 });
